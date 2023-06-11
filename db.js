3024cad5f44f9c4c484bbe3abfc6ebcca18b7484
Adil Abbas <adilabbas8092@gmail.com>
2023-06-11 11:21:58 -0700

    chnages in db file

--- db.js
+++ db.js
@@ -4,15 +4,17 @@ const dotenv = require("dotenv");
 const connect_to_db = async () => {
   dotenv.config();
   try {
-    await mongoose.connect(process.env.database_url, (err, connect) => {
-      if (err) {
-        console.log("Error in Mongoose Connect");
-        console.log(err);
-      } else {
-        console.log("connected to db successfullly");
-      }
-    });
-  } catch (error) {}
+    await mongoose
+      .connect(process.env.database_url)
+      .then((connection) => {
+        console.log("Connected to database Successfully");
+      })
+      .catch((error) => {
+        console.log("Error connecting to database");
+      });
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = connect_to_db;
