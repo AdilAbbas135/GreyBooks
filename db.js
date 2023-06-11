14170f8f927a0871ce914d25496ecf8cb133bb49
Adil Abbas <adilabbas8092@gmail.com>
2023-06-11 02:58:41 -0700

    first commit

---
+++ db.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+const connect_to_db = async (req, res) => {
+  dotenv.config();
+  try {
+    await mongoose.connect(process.env.database_url, (err, connect) => {
+      if (err) {
+        console.log("Error in Mongoose Connect");
+        console.log(err);
+      } else {
+        console.log("connected to db successfullly");
+      }
+    });
+  } catch (error) {}
+};
+
+module.exports = connect_to_db;
---
+++ index.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const express = require("express");
+const app = express();
+const connect_to_db = require("./db");
+var cors = require("cors");
+// var cookieParser = require("cookie-parser");
+const PORT = 8800;
+const server = http.createServer();
+
+app.listen(PORT, () => {
+  console.log("the app is running at port " + PORT);
+});
+
+// Middlewears
+connect_to_db();
+app.use(express.json());
+app.use(cors());
+// app.use(cookieParser());
+app.use("/uploads", express.static("uploads"));
