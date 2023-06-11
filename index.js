025a9058f331ae1863e68377d345714536b24c80
Adil Abbas <adilabbas8092@gmail.com>
2023-06-11 07:58:19 -0700

    changes done

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ const connect_to_db = require("./db");
 var cors = require("cors");
 // var cookieParser = require("cookie-parser");
 const PORT = 8800;
-const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "application/json" });
-  res.end("Hello World!");
-});
 
 app.listen(PORT, () => {
   console.log("the app is running at port " + PORT);
