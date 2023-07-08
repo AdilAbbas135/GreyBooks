9c0d77eceefc490a96fda07dd633c2aa63646fd9
Adil Abbas <adilabbas8092@gmail.com>
2023-07-08 12:37:05 -0700

    changes done final

--- Routes/find.js
+++ Routes/find.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const BooksModel = require("../Model/Books");
+const { default: mongoose } = require("mongoose");
 const router = express.Router();
 
+//FETCH ALL BOOK
 router.get("/books", async (req, res) => {
   try {
     const Books = await BooksModel.find();
@@ -9,6 +11,23 @@ router.get("/books", async (req, res) => {
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//FETCH ALL BOOKS OF A SPECIFIC CATEGORY
+router.get("/books/:category", async (req, res) => {
+  try {
+    if (mongoose.Types.ObjectId.isValid(req.params.category)) {
+      const Books = await BooksModel.find({ Category: req.params.category });
+      return res.status(200).json({ Books });
+    } else {
+      return res.status(404).json({ error: "Not Found" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ error: "Internal Server Error", msg: error.message });
   }
 });
 
