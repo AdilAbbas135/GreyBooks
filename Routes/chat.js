846bb4832f55ec46114eb31caaa07723e3ba086f
Adil Abbas <adilabbas8092@gmail.com>
2023-07-19 00:30:33 -0700

    changes are done

--- Routes/chat.js
+++ Routes/chat.js
@@ -3,6 +3,7 @@ const VerifyToken = require("../Middlewear/VerifyToken");
 const RoomModel = require("../Model/Rooms");
 const { default: mongoose } = require("mongoose");
 const MessagesModel = require("../Model/Messages");
+const ProfileModel = require("../Model/Profile");
 const router = express.Router();
 
 // FETCH ALL ROOMS OF A SPECIFIC USER
@@ -20,25 +21,58 @@ router.post("/all-rooms", VerifyToken, async (req, res) => {
 // FETCH ALL CHAT HISTORY OF A SPECIFIC SENDER AND RECIEVER
 router.post("/chat", VerifyToken, async (req, res) => {
   try {
+    const Room = await RoomModel.findOne({
+      Members: { $in: [req.user.profileId, req.body?.RecieverId] },
+    });
+
+    const Reciever = await ProfileModel.findById(req.body?.RecieverId);
     const AllChats = await MessagesModel.aggregate([
       {
         $match: {
-          SenderId: req.user.profileId || req.body.RecieverId,
-          RecieverId: req.user.profileId || req.body.RecieverId,
+          RoomId: Room?._id,
         },
       },
       {
         $lookup: {
           from: "profiles",
-          localField: req.body.RecieverId,
+          localField: "RecieverId",
           foreignField: "_id",
           as: "Reciever",
         },
       },
     ]);
-
-    return res.status(200).json(AllChats);
+    return res.status(200).json({ AllChats, Reciever });
   } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+//SEND A MESSAGE
+router.post("/send-message", VerifyToken, async (req, res) => {
+  try {
+    let FinalRoom;
+    const Room = await RoomModel.findOne({
+      Members: { $in: [req.user.profileId, req.body.RecieverId] },
+    });
+    console.log("room is");
+    console.log(Room);
+    if (!Room._id) {
+      FinalRoom = await RoomModel.create({
+        Members: [req.user.profileId, req.body.RecieverId],
+      });
+    } else {
+      FinalRoom = Room;
+    }
+    await MessagesModel.create({
+      RoomId: FinalRoom._id,
+      SenderId: req.user.profileId,
+      RecieverId: req.body.RecieverId,
+      Message: req.body.Message,
+    });
+    return res.status(200).json({ msg: "Message Sent Successfully" });
+  } catch (error) {
+    console.log(error);
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
